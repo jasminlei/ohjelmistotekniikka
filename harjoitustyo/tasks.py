31688ca866035599624b73e0ad92772c0d6ec171
jasminlei <jasminleinonen@hotmail.com>
2025-04-01 11:27:21 +0300

    build task

--- harjoitustyo/tasks.py
+++ harjoitustyo/tasks.py
@@ -16,3 +16,8 @@ def coverage_report(ctx):
     ctx.run("coverage run --branch -m pytest src", pty=True)
     ctx.run("coverage html", pty=True)
     ctx.run("open htmlcov/index.html", pty=True)
+
+
+@task
+def build(ctx):
+    ctx.run("python src/build.py", pty=True)
