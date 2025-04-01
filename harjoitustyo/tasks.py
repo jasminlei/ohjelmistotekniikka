0cb057b71520fc004db1e2c1588323f69a2fd248
jasminlei <jasminleinonen@hotmail.com>
2025-04-01 11:01:35 +0300

    invoke-tehtävät

---
+++ harjoitustyo/tasks.py
@@ -0,0 +1,18 @@
+from invoke import task
+
+
+@task
+def start(ctx):
+    ctx.run("python src/index.py", pty=True)
+
+
+@task
+def tests(ctx):
+    ctx.run("pytest src", pty=True)
+
+
+@task
+def coverage_report(ctx):
+    ctx.run("coverage run --branch -m pytest src", pty=True)
+    ctx.run("coverage html", pty=True)
+    ctx.run("open htmlcov/index.html", pty=True)
