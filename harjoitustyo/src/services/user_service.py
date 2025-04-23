1404dd5443c8b1b03bbd7d1a7db0b1dca8e17b3c
jasminlei <jasminleinonen@hotmail.com>
2025-04-23 10:12:07 +0300

    update user_service

--- harjoitustyo/src/services/user_service.py
+++ harjoitustyo/src/services/user_service.py
@@ -4,9 +4,22 @@ from entities.user import User
 
 class UserService:
     def __init__(self, repository):
+        """
+        Handles user-related actions like creating users.
+        """
         self._user_repository = repository
 
     def create_user(self, username, password):
+        """
+        Creates a new user if the username is not already taken.
+
+        Args:
+            username (str): Username.
+            password (str): Password.
+
+        Returns:
+            User: The created user object.
+        """
         existing_user = self._user_repository.find_by_username(username)
 
         if existing_user:
@@ -14,9 +27,6 @@ class UserService:
 
         user = self._user_repository.create(User(username, password))
         return user
-
-    def get_all_users(self):
-        return self._user_repository.find_all()
 
 
 user_service = UserService(user_repository)
