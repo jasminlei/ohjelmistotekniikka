8ec555181bdac34297f449c9eec43efdce36697b
jasminlei <jasminleinonen@hotmail.com>
2025-05-05 13:48:11 +0300

    add .env variables for database connection

--- harjoitustyo/src/database_connection.py
+++ harjoitustyo/src/database_connection.py
@@ -1,18 +1,20 @@
 import os
 import sqlite3
+from dotenv import load_dotenv
+
+load_dotenv()
 
 dirname = os.path.dirname(__file__)
 
 
 def get_database_connection(test=False):
-    if test:
-        connection = sqlite3.connect(
-            os.path.join(dirname, "..", "data", "test_database.sqlite")
-        )
-    else:
-        connection = sqlite3.connect(
-            os.path.join(dirname, "..", "data", "database.sqlite")
-        )
+    db_filename = (
+        os.environ.get("TEST_DATABASE_FILENAME", "test_database.sqlite")
+        if test
+        else os.environ.get("DATABASE_FILENAME", "database.sqlite")
+    )
 
+    db_path = os.path.join(dirname, "..", "data", db_filename)
+    connection = sqlite3.connect(db_path)
     connection.row_factory = sqlite3.Row
     return connection
