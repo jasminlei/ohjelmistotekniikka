734e8dd0d9720ac76c566a094fd96d87aa738315
jasminlei <jasminleinonen@hotmail.com>
2025-05-05 13:45:27 +0300

    add error handling in userservice

--- harjoitustyo/src/services/user_service.py
+++ harjoitustyo/src/services/user_service.py
@@ -11,22 +11,27 @@ class UserService:
 
     def create_user(self, username, password):
         """
-        Creates a new user if the username is not already taken.
+        Creates a new user if the username is not already taken and meets length constraints.
 
         Args:
             username (str): Username.
             password (str): Password.
 
         Returns:
-            User: The created user object.
+            tuple: (bool, User|str),
+                   - True, User if successful
+                   - False, error message if failed
         """
+        if len(username) < 3 or len(username) > 30:
+            return False, "Käyttäjänimen tulee olla 3–30 merkkiä pitkä!"
+
         existing_user = self._user_repository.find_by_username(username)
 
         if existing_user:
-            raise ValueError(f"Käyttäjänimi {username} on jo varattu!")
+            return False, f"Käyttäjänimi {username} on jo varattu!"
 
         user = self._user_repository.create(User(username, password))
-        return user
+        return True, user
 
 
 user_service = UserService(user_repository)
